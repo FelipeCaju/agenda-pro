ef342cc5b8bc5ff41367f2e7ba7507f353a24ce1
Luiz Felipe <felipe22_172@hotmail.com>
2026-09-18 21:08:36 -0300

    fix: mantem menu lateral fixo e alinha versoes

--- config/version.php
+++ config/version.php
@@ -13,9 +13,9 @@ declare(strict_types=1);
  */
 const APP_VERSION_MAJOR = 1;
 const APP_VERSION_MINOR = 2;
-const APP_VERSION_PATCH = 8;
+const APP_VERSION_PATCH = 9;
 const APP_BUILD_DATE = '20260918';
-const APP_BUILD_NUMBER = 10;
+const APP_BUILD_NUMBER = 11;
 
 function app_version(): string
 {
